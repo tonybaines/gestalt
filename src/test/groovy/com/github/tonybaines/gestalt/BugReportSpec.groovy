b286593c93f04c7a3c71316777008944c4a9edf5
Tony Baines <tony.baines.1@gmail.com>
2014-12-23 21:59:31 +0000

    Reproducing (part of Issue #10)

--- src/test/groovy/com/github/tonybaines/gestalt/BugReportSpec.groovy
+++ src/test/groovy/com/github/tonybaines/gestalt/BugReportSpec.groovy
@@ -1,10 +1,43 @@
 package com.github.tonybaines.gestalt
 
-import spock.lang.Ignore
 import spock.lang.Specification
 
-
-@Ignore
 class BugReportSpec extends Specification {
 
+  interface A {
+    B getB()
+  }
+
+  interface B {
+    C getC()
+  }
+
+  interface C {
+    String getFoo()
+  }
+
+  def "Issue 10: NPE when serialising a config instance with null/missing values [Properties serialisation]"() {
+    given:
+    A configInstance = new A() {
+      B getB() { return null }
+    }
+    when:
+    Configurations.toProperties(configInstance, A)
+
+    then:
+    thrown(NullPointerException)
+
+  }
+
+  def "Issue 10: NPE when serialising a config instance with null/missing values [XML Serialisation]"() {
+    given:
+    A configInstance = Configurations.definedBy(A).without(Configurations.Feature.ExceptionOnNullValue, Configurations.Feature.Validation)
+      .fromXml(new StringBufferInputStream("""<a><b><c/></b></a>""")).done()
+    when:
+    Configurations.toXml(configInstance, A)
+
+    then:
+    thrown(NullPointerException)
+
+  }
 }
