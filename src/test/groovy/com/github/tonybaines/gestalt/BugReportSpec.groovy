ab2d26ddf4f6df1c71e0762e8a6d83e3554eae88
Tony Baines <tony.baines.1@gmail.com>
2014-12-27 12:17:47 +0000

    Moved test to a more appropriate place

--- src/test/groovy/com/github/tonybaines/gestalt/BugReportSpec.groovy
+++ src/test/groovy/com/github/tonybaines/gestalt/BugReportSpec.groovy
@@ -3,26 +3,5 @@ package com.github.tonybaines.gestalt
 import spock.lang.Specification
 
 class BugReportSpec extends Specification {
-  public interface Eventing {
-    MetricsConfig getMetrics()
-  }
-  interface MetricsConfig {
-//    @Default.Boolean(false)
-    boolean isJvmGaugesEnabled();
-  }
 
-  def "Issue 12: NPE when accessing a missing primitive value"() {
-    given:
-    Eventing configInstance = Configurations.definedBy(Eventing)
-      .without(Configurations.Feature.Validation)
-      .without(Configurations.Feature.ExceptionOnNullValue)
-      .fromXml(new ByteArrayInputStream('<Eventing/>'.bytes))
-      .done()
-
-    when:
-    configInstance.metrics.isJvmGaugesEnabled() == null
-
-    then:
-    thrown(NullPointerException)
-  }
 }
--- src/test/groovy/com/github/tonybaines/gestalt/MiscFeaturesSpec.groovy
+++ src/test/groovy/com/github/tonybaines/gestalt/MiscFeaturesSpec.groovy
@@ -151,4 +151,27 @@ class MiscFeaturesSpec extends Specification {
     e.message.contains('No valid sources configured')
   }
 
+
+  public interface Eventing {
+    MetricsConfig getMetrics()
+  }
+  interface MetricsConfig {
+    boolean isJvmGaugesEnabled();
+  }
+
+  def "Issue 12: NPE when accessing a missing primitive value"() {
+    given:
+    Eventing configInstance = Configurations.definedBy(Eventing)
+      .without(Configurations.Feature.ExceptionOnNullValue)
+      .fromXml(new ByteArrayInputStream('<Eventing/>'.bytes))
+      .done()
+
+    when:
+    configInstance.metrics.isJvmGaugesEnabled() == null
+
+    then:
+    thrown(NullPointerException)
+  }
+
+
 }
