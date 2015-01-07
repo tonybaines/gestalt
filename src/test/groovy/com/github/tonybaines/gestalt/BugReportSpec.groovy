6fcfe2bf02c4cce683dce0879aa7c35508a94408
Tony Baines <tony.baines.1@gmail.com>
2015-01-07 20:53:56 +0000

    Spec reproducing Issue 17 - False boolean value is treated as not found

--- src/test/groovy/com/github/tonybaines/gestalt/BugReportSpec.groovy
+++ src/test/groovy/com/github/tonybaines/gestalt/BugReportSpec.groovy
@@ -3,5 +3,23 @@ package com.github.tonybaines.gestalt
 import spock.lang.Specification
 
 class BugReportSpec extends Specification {
+  interface Truth {
+    boolean getBool()
+  }
 
+  def "Issue 17: False boolean value is treated as not found"() {
+    given:
+    Properties props = new Properties()
+    props.setProperty('bool', 'false')
+    Truth configInstance = Configurations.definedBy(Truth)
+      .fromProperties(props)
+      .done()
+
+    when:
+    configInstance.getBool() == false
+
+    then:
+    thrown ConfigurationException
+
+  }
 }
