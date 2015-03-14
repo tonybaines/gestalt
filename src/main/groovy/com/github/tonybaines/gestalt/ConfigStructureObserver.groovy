672e983cd2c5da129e56e854f3aba905f0a07fb0
Tony Baines <tony.baines.1@gmail.com>
2015-03-14 18:33:41 +0000

    Convert the Java ConfigStructureObserver interface to Groovy to make Gradle joint compilation work

---
+++ src/main/groovy/com/github/tonybaines/gestalt/ConfigStructureObserver.groovy
@@ -0,0 +1,5 @@
+package com.github.tonybaines.gestalt
+
+interface ConfigStructureObserver {
+  void property(ConfigProperty configProperty)
+}
--- src/main/java/com/github/tonybaines/gestalt/ConfigStructureObserver.java
+++
@@ -1,5 +0,0 @@
-package com.github.tonybaines.gestalt;
-
-public interface ConfigStructureObserver {
-  void property(ConfigProperty configProperty);
-}
