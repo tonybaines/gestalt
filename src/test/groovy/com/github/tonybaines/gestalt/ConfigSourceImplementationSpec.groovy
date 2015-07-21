5c460170e25e97610ae2418e62019fd760d1f2e6
Tony Baines <tony.baines.1@gmail.com>
2015-07-21 10:27:48 +0100

    Update ConfigSourceImplementationSpec.groovy

--- src/test/groovy/com/github/tonybaines/gestalt/ConfigSourceImplementationSpec.groovy
+++ src/test/groovy/com/github/tonybaines/gestalt/ConfigSourceImplementationSpec.groovy
@@ -4,19 +4,6 @@ import spock.lang.Specification
 
 import java.lang.reflect.Method
 
-/*============================================================================*
- * - COPYRIGHT NOTICE -
- *
- * Copyright (c) British Telecommunications plc, 2010, All Rights Reserved
- *
- * The information contained herein and which follows is proprietary
- * information and is the property of BT. This information is not to be
- * divulged, released, copied, reproduced, or conveyed to unauthorised
- * personnel,companies or other institutions without the direct and expressed
- * approval in writing of BT
- *
- *============================================================================*/
-
 class ConfigSourceImplementationSpec extends Specification {
   def "Consuming a simple externally implemented config source" ( ) {
     given:
