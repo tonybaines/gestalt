387b54faa4de82d32ca3f5afdc09bf2e00e40931
Tony Baines <tony.baines.1@gmail.com>
2019-01-31 20:35:15 +0000

    Fixed failing specs (re-structure the data driven tests)

--- src/test/groovy/com/github/tonybaines/gestalt/ValidationSpec.groovy
+++ src/test/groovy/com/github/tonybaines/gestalt/ValidationSpec.groovy
@@ -11,127 +11,129 @@ import static com.github.tonybaines.gestalt.Fixture.newCompositeConfiguration
 
 class ValidationSpec extends Specification {
 
-  def "If the default value is used and it breaks validation constraints, it is ignored (null returned)"() {
-    given:
-    TestConfig configuration = newCompositeConfiguration()
+    def "If the default value is used and it breaks validation constraints, it is ignored (null returned)"() {
+        given:
+        TestConfig configuration = newCompositeConfiguration()
 
-    when:
-    configuration.getStringValueWhoseDefaultBreaksValidation()
+        when:
+        configuration.getStringValueWhoseDefaultBreaksValidation()
 
-    then:
-    def e = thrown(ConfigurationException)
-    e.message.contains('Failed to handle getStringValueWhoseDefaultBreaksValidation')
-  }
+        then:
+        def e = thrown(ConfigurationException)
+        e.message.contains('Failed to handle getStringValueWhoseDefaultBreaksValidation')
+    }
 
-  def "If the default value is used and it breaks validation constraints, it is ignored (null returned) in a sub-interface"() {
-    given:
-    TestConfig configuration = newCompositeConfiguration()
+    def "If the default value is used and it breaks validation constraints, it is ignored (null returned) in a sub-interface"() {
+        given:
+        TestConfig configuration = newCompositeConfiguration()
 
-    when:
-    configuration.getStringValueWhoseDefaultBreaksValidation()
+        when:
+        configuration.getStringValueWhoseDefaultBreaksValidation()
 
-    then:
-    def e = thrown(ConfigurationException)
-    e.message.contains('Failed to handle getStringValueWhoseDefaultBreaksValidation')
-  }
+        then:
+        def e = thrown(ConfigurationException)
+        e.message.contains('Failed to handle getStringValueWhoseDefaultBreaksValidation')
+    }
 
-  def "An invalid configured value in all sources will fall-back to a default"() {
-    given:
-    TestConfig configuration = newCompositeConfiguration()
+    def "An invalid configured value in all sources will fall-back to a default"() {
+        given:
+        TestConfig configuration = newCompositeConfiguration()
 
-    expect:
-    configuration.getSubConfig().getValueWhichIsDefinedToBreakValidationButHasADefault() == "fin"
-  }
+        expect:
+        configuration.getSubConfig().getValueWhichIsDefinedToBreakValidationButHasADefault() == "fin"
+    }
 
-  def "Validating an entire instance"() {
-    given:
-    TestConfig configuration = Configurations.definedBy(TestConfig)
-      .fromPropertiesResource('common.properties')
-      .done()
+    def "Validating an entire instance"() {
+        given:
+        TestConfig configuration = Configurations.definedBy(TestConfig)
+                .fromPropertiesResource('common.properties')
+                .done()
 
-    when:
-    ValidationResult validationResult = Configurations.validate(configuration, TestConfig)
-    println validationResult
+        when:
+        ValidationResult validationResult = Configurations.validate(configuration, TestConfig)
+        println validationResult
 
-    then:
-    validationResult.hasFailures()
-    validationResult.any { it.property == 'stringValueWhoseDefaultBreaksValidation' }
-    validationResult.any { it.property == 'integerThatIsTooLarge' }
-    validationResult.any { it.property == 'subConfig.booleanValueWhoseValueBreaksValidation'}
-    validationResult.any { it.property == 'subConfig.l2.nonExistent'}
-    validationResult.any { it.property == 'custom-validation' && it.message == 'foobar'}
-  }
+        then:
+        validationResult.hasFailures()
+        validationResult.any { it.property == 'stringValueWhoseDefaultBreaksValidation' }
+        validationResult.any { it.property == 'integerThatIsTooLarge' }
+        validationResult.any { it.property == 'subConfig.booleanValueWhoseValueBreaksValidation' }
+        validationResult.any { it.property == 'subConfig.l2.nonExistent' }
+        validationResult.any { it.property == 'custom-validation' && it.message == 'foobar' }
+    }
 
-  @Unroll
-  def "Issue #7: Validating an instance where a mandatory value is null for #configInterface"() {
-    when:
-    ValidationResult validationResult = Configurations.validate(configuration, configInterface)
-    println validationResult
+    @Unroll
+    def "Issue #7: Validating an instance where a mandatory value is null for #configInterface"() {
+        when:
+        ValidationResult validationResult =
+                Configurations.validate(Configurations.definedBy(configInterface).fromProperties(new Properties()).done(),
+                        configInterface)
+        println validationResult
 
-    then:
-    validationResult.hasFailures()
+        then:
+        validationResult.hasFailures()
 
-    where:
-    configuration                                                                                   | configInterface
-    Configurations.definedBy(Wrapper).fromProperties(new Properties()).done()                       | Wrapper
-    Configurations.definedBy(ValidatingANullMandatoryValue).fromProperties(new Properties()).done() | ValidatingANullMandatoryValue
+        where:
+        configInterface               | _
+        Wrapper                       | _
+        ValidatingANullMandatoryValue | _
 
-  }
+    }
 
-  def "Issue 19: Validating a custom type"() {
-    given:
-    CustomType config = Configurations.definedBy(CustomType).fromProperties(new Properties()).done()
-    when:
-    ValidationResult validationResult = Configurations.validate(config, CustomType)
+    def "Issue 19: Validating a custom type"() {
+        given:
+        CustomType config = Configurations.definedBy(CustomType).fromProperties(new Properties()).done()
+        when:
+        ValidationResult validationResult = Configurations.validate(config, CustomType)
 
-    then:
-    !validationResult.hasFailures()
-  }
+        then:
+        !validationResult.hasFailures()
+    }
 
-  def "Validating with a custom validator (multiple validations)"() {
-    def properties = new Properties()
-    properties.put('foo', 'foo')
-    properties.put('bar', 'bar')
-    given:
-    CustomValidationConfig config = Configurations.definedBy(CustomValidationConfig).fromProperties(properties).done()
-    when:
-    ValidationResult validationResult = Configurations.validate(config, CustomValidationConfig)
+    def "Validating with a custom validator (multiple validations)"() {
+        def properties = new Properties()
+        properties.put('foo', 'foo')
+        properties.put('bar', 'bar')
+        given:
+        CustomValidationConfig config = Configurations.definedBy(CustomValidationConfig).fromProperties(properties).done()
+        when:
+        ValidationResult validationResult = Configurations.validate(config, CustomValidationConfig)
 
-    then:
-    validationResult.hasFailures()
-    validationResult.items.size() == 2
-    validationResult.items[0].toString().contains('Only Foo *or* Bar should be defined')
-  }
+        then:
+        validationResult.hasFailures()
+        validationResult.items.size() == 2
+        validationResult.items[0].toString().contains('Only Foo *or* Bar should be defined')
+    }
 
-  def "Validating with a custom validator (single validation)"() {
-    def properties = new Properties()
-    properties.put('foo', 'baz')
-    properties.put('baz', 'baz')
-    given:
-    CustomValidationConfig config = Configurations.definedBy(CustomValidationConfig).fromProperties(properties).done()
-    when:
-    ValidationResult validationResult = Configurations.validate(config, CustomValidationConfig)
+    def "Validating with a custom validator (single validation)"() {
+        def properties = new Properties()
+        properties.put('foo', 'baz')
+        properties.put('baz', 'baz')
+        given:
+        CustomValidationConfig config = Configurations.definedBy(CustomValidationConfig).fromProperties(properties).done()
+        when:
+        ValidationResult validationResult = Configurations.validate(config, CustomValidationConfig)
 
-    then:
-    validationResult.hasFailures()
-    validationResult.items.size() == 1
-    validationResult.items[0].toString().contains("foo cannot be 'baz' if baz is also 'baz'")
-  }
+        then:
+        validationResult.hasFailures()
+        validationResult.items.size() == 1
+        validationResult.items[0].toString().contains("foo cannot be 'baz' if baz is also 'baz'")
+    }
 
-  interface CustomType {
-    @Default.String("foo")
-    @NotNull
-    ObfuscatedString getObfuscatedString()
-  }
+    interface CustomType {
+        @Default.String("foo")
+        @NotNull
+        ObfuscatedString getObfuscatedString()
+    }
 
-  interface Wrapper {
-    public ValidatingANullMandatoryValue getSubLevel()
-  }
+    interface Wrapper {
+        public ValidatingANullMandatoryValue getSubLevel()
+    }
 
-  interface ValidatingANullMandatoryValue {
-    @NotNull
-    public Integer getPort()
-  }
+    interface ValidatingANullMandatoryValue {
+        @NotNull
+        public Integer getPort()
+    }
 
 
 }
